8d2979a026d9be624ac79a0f33e5cc477a0a6a2a
Kamaleeswary <kamaleeanbu@gmail.com>
2021-03-29 17:35:50 +0530

    Issue SB:23035 feature: downgraded the angular version from 8 to 7

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: 'discussion-forum' , loadChildren: './wrapper/wrapper.module#WrapperModule',
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
